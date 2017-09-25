6159fbafc2aa2507e98fa19d45cc2e7766761474
mccrodp <info@opensourceopenmind.ninja>
2017-09-25 16:29:49 +0900

    Added Express, NPM and gitignore.

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('This is working!!!')
+})
+
+var server = app.listen(3000, function () {
+  console.log('Server running on http://localhost:' + server.address().port)
+})
