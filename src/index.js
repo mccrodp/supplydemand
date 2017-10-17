036fb3a19169cbc20259eec5255522d0e4147b1d
mccrodp <info@opensourceopenmind.ninja>
2017-10-17 17:50:34 +0900

    Added Webpack, Babel and React: starting to configure.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-var express = require('express')
-var app = express()
+import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var DB = require('./db/bookshelf')
+function component() {
+  var element = document.createElement('div');
 
-app.get('/', function (req, res) {
-  res.send('This is working!!!')
-})
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-var server = app.listen(3000, function () {
-  console.log('Server running on http://localhost:' + server.address().port)
-})
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+// var express = require('express')
+// var app = express()
+
+// var DB = require('./db/bookshelf')
+
+// app.get('/', function (req, res) {
+//   res.send('This is working!!!')
+// })
+
+// var server = app.listen(3000, function () {
+//   console.log('Server running on http://localhost:' + server.address().port)
+// })
